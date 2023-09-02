176d0aa6cc74de83e0b297c4d420f1a2945ae7ff
Marcus Davanço <md.daves@gmail.com>
2023-09-02 15:21:36 -0300

    chore: eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,9 +10,8 @@ const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('Invalid environment variables', _env.error.format())
-  
+
   throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
-
