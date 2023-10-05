b17d67354ff7978c5b4c178a795612fff31f313e
Marcus Davanço <md.daves@gmail.com>
2023-10-05 13:04:34 -0300

    chore: session route

--- src/http/controllers/org/routes.ts
+++ src/http/controllers/org/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { find } from './find'
 import { register } from './register'
+import { authenticate } from './authenticate'
 
 export async function orgRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.get('/orgs/:org', find)
+
+  app.post('/sessions', authenticate)
 }
