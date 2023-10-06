f23bd374db97db6c20431d86ec70cc878dbd54bd
Marcus Davanço <md.daves@gmail.com>
2023-10-06 17:33:33 -0300

    tests: register org e2e

---
+++ src/http/controllers/org/register.spec.ts
@@ -0,0 +1,24 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+
+describe('Register an Org(e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      name: 'Legatum',
+      address: 'Rua Ali Perto, 123',
+      phone: '(11) 9999-9999',
+      email: 'legatum@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
