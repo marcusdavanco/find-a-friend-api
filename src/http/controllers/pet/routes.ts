1c90c2426a37a87435d32234fd182dcd0064ee20
Marcus Davanço <md.daves@gmail.com>
2023-10-07 14:50:08 -0300

    feat: auth verification middleware

--- src/http/controllers/pet/routes.ts
+++ src/http/controllers/pet/routes.ts
@@ -3,10 +3,12 @@ import { search } from './search'
 import { details } from './details'
 import { fetch } from './fetch'
 import { register } from './register'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function petRoutes(app: FastifyInstance) {
-  app.post('/pets', register)
   app.get('/pets/city/:city', fetch)
   app.get('/pets/:pet', details)
   app.get('/pets/search', search)
+
+  app.post('/pets', { onRequest: [verifyJWT] }, register)
 }
